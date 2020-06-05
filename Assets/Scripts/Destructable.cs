7aa30c6d97e65fc9e8a443df821891fddfe5d335
alextitov19 <54718346+alextitov19@users.noreply.github.com>
2020-06-05 15:25:55 -0700

    break working

---
+++ Assets/Scripts/Destructable.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class Destructable : MonoBehaviour
+{
+    public GameObject destroyedVersion;
+    public void swapToBroken()
+    {
+        Debug.Log("Enter swaptobroken");
+        Instantiate(destroyedVersion, transform.position, transform.rotation);
+        Destroy(gameObject);
+        Debug.Log("Exit swaptobroken");
+    }
+}
--- Assets/Scripts/PlayerColiision.cs
+++ Assets/Scripts/PlayerColiision.cs
@@ -2,11 +2,12 @@
 
 public class PlayerColiision : MonoBehaviour {
 
-
+    public Destructable destructable;
        void OnCollisionEnter (Collision collisionInfo) 
         {
             if (collisionInfo.collider.tag == "Obstacle")
              {
+                FindObjectOfType<Destructable>().swapToBroken();
                 GetComponent<PlayerMovement>().enabled = false;
                 FindObjectOfType<GameManager>().endGame();
              }
