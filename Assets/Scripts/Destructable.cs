39f06b800e316b7f7181216afea9489c442010da
alextitov19 <54718346+alextitov19@users.noreply.github.com>
2020-06-05 15:41:44 -0700

    break works

--- Assets/Scripts/Destructable.cs
+++ Assets/Scripts/Destructable.cs
@@ -3,10 +3,11 @@
 public class Destructable : MonoBehaviour
 {
     public GameObject destroyedVersion;
+    public Vector3 translocation;
     public void swapToBroken()
     {
         Debug.Log("Enter swaptobroken");
-        Instantiate(destroyedVersion, transform.position, transform.rotation);
+        Instantiate(destroyedVersion, transform.position + translocation, transform.rotation);
         Destroy(gameObject);
         Debug.Log("Exit swaptobroken");
     }
