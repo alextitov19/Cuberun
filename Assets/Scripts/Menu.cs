44426487e8499585ada5e9df328ac41e5b3e5280
alextitov19 <54718346+alextitov19@users.noreply.github.com>
2020-08-25 19:20:30 -0700

    added levels

--- Assets/Scripts/Menu.cs
+++ Assets/Scripts/Menu.cs
@@ -33,4 +33,17 @@ public class Menu : MonoBehaviour
     {
         SceneManager.LoadScene("Store");
     }
+    public void ResetClicked()
+    {
+        player.perk2xAmount = 0;
+        player.perk2xSelected = 0;
+        player.perkInvinvibilityAmount = 0;
+        player.perkInvinvibilitySelected = 0;
+        player.perkSlowMoAmount = 0;
+        player.perkSlowMoSelected = 0;
+        player.currentLevel = 1;
+        player.coins = 0;
+        player.xp = 0;
+        player.SaveData();
+    }
 }
