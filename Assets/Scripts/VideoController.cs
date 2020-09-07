9475f22fe1d14660e4b4b0d95acbeb39f42e23bb
alextitov19 <54718346+alextitov19@users.noreply.github.com>
2020-09-06 20:48:09 -0700

    added video player

---
+++ Assets/Scripts/VideoController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+using UnityEngine.SceneManagement;
+
+public class VideoController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public VideoPlayer VideoPlayer; // Drag & Drop the GameObject holding the VideoPlayer component
+    void Start()
+    {
+        VideoPlayer.loopPointReached += LoadScene;
+    }
+    void LoadScene(VideoPlayer vp)
+    {
+        SceneManager.LoadScene("Menu");
+    }
+}
